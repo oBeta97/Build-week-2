cfc55042f81fea1c6481ed1bf414714d5e7567a1
Pietroro98 <pietroro98@gmail.com>
2024-08-01 11:11:27 +0200

    modifiche finali del media player

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -80,17 +80,27 @@ function startTimer() {
 
 function updateTimeDisplays() {
   if (audio) {
-    console.log(audio.currentTime)
-    console.log(audio.duration)
-    const currentTime = audio.currentTime;
-    const duration = audio.duration.slice(0, audio.duration.indexOf('.'))
+    const currentTime = formatTime(audio.currentTime);
+    const duration = formatTime(audio.duration);
     document.getElementById("currentTime").innerText = currentTime;
     document.getElementById("totalTime").innerText = duration;
-    
+    console.log(audio.currentTime);
+    console.log(audio.duration);
   }
 }
 
+ function formatTime(seconds) {
+   const minutes = Math.floor(seconds / 60);
+   const remainingSeconds = Math.floor(seconds % 60)
+   // Aggiungi uno zero davanti ai secondi se è un singolo numero
+   const formattedSeconds =
+     remainingSeconds < 10 ? "0" + remainingSeconds : remainingSeconds;
+   return minutes + ":" + formattedSeconds;
+ }
 
-
-
-
+ 
+// function formatTime(seconds) {
+//   const minutes = Math.floor(seconds / 60);
+//   const seconds = Math.floor(seconds % 60);
+//   return `${minutes}:${seconds < 10 ? "0" : ""} ${seconds}`;
+// }
