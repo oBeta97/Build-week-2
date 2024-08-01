d321eb3c01bf1678b708080911203477c60ada82
Pietroro98 <pietroro98@gmail.com>
2024-08-01 10:46:03 +0200

    modifiche mediaplayer

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -7,72 +7,90 @@
 
 let audio = null;
 let timer = null;
-const playButton = document.getElementById('playButton')
-const playCenter = document.getElementById('playCenter')
+const playButton = document.getElementById("playButton");
+const playCenter = document.getElementById("playCenter");
 
-playButton.addEventListener('click', function() {
+playButton.addEventListener("click", function () {
+  if (audio && !audio.paused) {
+    audio.pause();
+    this.innerHTML = `<span class="px-2 text-white text-nowrap small">play</span>`;
+    playCenter.src = "./assets/imgs/icons/play_musicbar.png";
+    // console.log('Audio paused');
+  } else {
+    const audioUrl = this.getAttribute("data-previewSong");
+    if (!audio) {
+      audio = new Audio(audioUrl);
+    }
+    audio.play();
+    startTimer();
+    this.innerHTML = `<span class="px-2 text-white text-nowrap small">pausa</span>`;
+    playCenter.src = "./assets/imgs/icons/pause_musicbar.png";
+    // console.log('Playing audio:', audioUrl);
+  }
+});
 
+playCenter.addEventListener("click", function () {
+  if (audio && !audio.paused) {
+    audio.pause();
+    playButton.innerHTML = `<span class="px-2 text-white text-nowrap small">play</span>`;
+    playCenter.src = "./assets/imgs/icons/play_musicbar.png";
+  } else {
+    const audioUrl = this.getAttribute("data-previewSong");
+    if (!audio) {
+      audio = new Audio(audioUrl);
+    }
+    audio.play();
+    startTimer();
+    playButton.innerHTML = `<span class="px-2 text-white text-nowrap small">pausa</span>`;
+    playCenter.src = "./assets/imgs/icons/pause_musicbar.png";
+    // console.log('Playing audio:', audioUrl);
+  }
+});
+
+// prendo id del imput del volume e creo la funzione
+document.getElementById("volumeSlider").addEventListener("input", function () {
+  if (audio) {
+    audio.volume = this.value / 100;
+    // console.log(this.value);
+  }
+});
+
+document
+  .getElementById("timelineSlider")
+  .addEventListener("input", function () {
+    if (audio) {
+      const value = this.value;
+      const duration = audio.duration;
+      audio.currentTime = (value / 100) * duration;
+      updateTimeDisplays();
+    }
+  });
+
+function startTimer() {
+  timer = setInterval(() => {
+    if (audio) {
+      updateTimeDisplays();
+      const duration = audio.duration;
+      const currentTime = audio.currentTime;
+      document.getElementById("timelineSlider").value =
+        (currentTime / duration) * 100;
+    }
+  }, 1000); // intervallo che esegue la funzione ogni secondo
+}
+
+function updateTimeDisplays() {
+  if (audio) {
+    console.log(audio.currentTime)
+    console.log(audio.duration)
+    const currentTime = audio.currentTime;
+    const duration = audio.duration.slice(0, audio.duration.indexOf('.'))
+    document.getElementById("currentTime").innerText = currentTime;
+    document.getElementById("totalTime").innerText = duration;
     
-      if (audio && !audio.paused) {
-        audio.pause();
-        this.innerHTML = `<span class="px-2 text-white text-nowrap small">play</span>`
-        playCenter.src = "./assets/imgs/icons/play_musicbar.png"
-        // console.log('Audio paused');
-      } else {
-        const audioUrl = this.getAttribute('data-previewSong');
-        if (!audio) {
-          audio = new Audio(audioUrl);
-        }
-        audio.play();
-        startTimer();
-        this.innerHTML = `<span class="px-2 text-white text-nowrap small">pausa</span>`
-        playCenter.src = "./assets/imgs/icons/pause_musicbar.png"
-        // console.log('Playing audio:', audioUrl);
-      }
-    });
-
-    playCenter.addEventListener('click', function() {
-        if (audio && !audio.paused) {
-            audio.pause();
-            this.src = "./assets/imgs/icons/play_musicbar.png"
-            this.innerHTML = `<span class="px-2 text-white text-nowrap small">play</span>`
-
-          } else {
-            const audioUrl = this.getAttribute('data-previewSong');
-            if (!audio) {
-              audio = new Audio(audioUrl);
-            }
-            audio.play();
-            startTimer();
-            this.src = "./assets/imgs/icons/pause_musicbar.png"
-            this.innerHTML = `<span class="px-2 text-white text-nowrap small">pausa</span>`
-            // console.log('Playing audio:', audioUrl);
-          }
-    })
+  }
+}
 
 
 
-    // prendo id del imput del volume e creo la funzione
-    document.getElementById('volumeSlider').addEventListener('input', function() {
-        if (audio) {
-          audio.volume = this.value / 100;
-          // console.log(this.value);
-        }
-      });
 
-      document.getElementById('timelineSlider').addEventListener('input', function() {
-        if (audio) {
-          const value = this.value;
-          const duration = audio.duration;
-          audio.currentTime = (value / 100) * duration;
-          updateTimeDisplays();
-        }
-      });
 
-      function startTimer() {
-        timer = setInterval(() =>{
-
-        })
-      }
-        
-      
